e594d9ad30b03c36f39a8d432f33a30a0b85fbda
Shikhar_Bansal <shikharbansal007@gmail.com>
2020-12-11 20:30:23 +0530

    corrected code v3

--- codeial/routes/users.js
+++ codeial/routes/users.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile', passport.checkAuthentication ,usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
@@ -12,11 +12,13 @@ router.get('/sign-in', usersController.signIn);
 
 router.post('/create', usersController.create);
 
-//use passport as  a middleware to authenticate
+// use passport as a middleware to authenticate
 router.post('/create-session', passport.authenticate(
     'local',
-    {failureRedirect: '/users/sign-in' },
- ), usersController.createSession);
+    {failureRedirect: '/users/sign-in'},
+), usersController.createSession);
 
+
+router.get('/sign-out', usersController.destroySession);
 
 module.exports = router;
